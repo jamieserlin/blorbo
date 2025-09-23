cb2c52d98c1f22cce9f25ea257ff311aa1501376
Fathi Theolaire <fathi2.theolaire@live.uwe.ac.uk>
2025-09-23 19:53:40 +0100

    Start of hydration system

---
+++ hydration.gd
@@ -0,0 +1,14 @@
+extends Control
+
+var Thirst = 100
+var MaxThirst = 100
+var MinThirst = 0
+
+func init_Thirst(_Thirst):
+	var Thirst = 100
+	var MaxThirst = 100
+	var MinThirst = 0
+
+func _on_timer_timeout() -> void:
+	
+	pass # Replace with function body.
