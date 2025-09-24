fa011e98ee1ac3340f327aac6cf036e77ec52480
Fathi Theolaire <fathi2.theolaire@live.uwe.ac.uk>
2025-09-24 15:31:54 +0100

    minor updates to Hydration

--- hydration.gd
+++ hydration.gd
@@ -1,14 +1,16 @@
 extends Control
-
 var Thirst = 100
 var MaxThirst = 100
 var MinThirst = 0
 
-func init_Thirst(_Thirst):
-	var Thirst = 100
-	var MaxThirst = 100
-	var MinThirst = 0
+func _ready() -> void:
+	$Timer.start()
+	print("Initalised")
+	
+func _process(delta: float) -> void:
+	Thirst = 2 * $Timer.time_left
+	$ProgressBar.value_changed(Thirst)
 
 func _on_timer_timeout() -> void:
+	print("Timer Stop")
 	
-	pass # Replace with function body.
