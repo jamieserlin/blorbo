76748eada2f7dc8c6f25565e5b58ed9a193d1ccd
jamieserlin <James2.Serlin@live.uwe.ac.uk>
2025-09-24 22:23:48 +0100

    half done push

--- npc/hippocampus/climb.gd
+++ npc/hippocampus/climb.gd
@@ -1,15 +1,8 @@
 extends Node3D
 #create a dialogue file in dialogue button above lol
-var resource = load("res://npc/hippocampus/hippocampus.dialogue")
-const my_scene = preload("res://npc/hippocampus_dialogue.tscn")
 var in_body = false
 var balloon
-#use the example balloon to create an interaction
-#nready var balloon = $ExampleBalloon
-
-# dialogue resource? idfk//// dialogue start is the title of the dialogue
-@export var dialogue_resource: DialogueResource
-@export var dialogue_start: String = "start"
+var player_body
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:	
@@ -19,16 +12,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Interact") && in_body:
-			var new_enemy = my_scene.instantiate()
-			add_child(new_enemy)
-			balloon = get_tree().get_first_node_in_group("hippo_dialogue")
-			if balloon != null:
-				balloon.start(resource, dialogue_start)
+		pass
 	pass
 
 
 func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
+		player_body = body
 		in_body = true
 		print("FUCK")
 		
