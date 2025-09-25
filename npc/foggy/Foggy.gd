1f306c4687cea6b057d28bb0f7c32c57fb682ea4
jamie <james2.serlin@live.uwe.ac.uk>
2025-09-25 22:14:41 +0100

    foggy !!!

---
+++ npc/foggy/Foggy.gd
@@ -0,0 +1,41 @@
+extends Node3D
+#create a dialogue file in dialogue button above lol
+var resource = load("res://npc/foggy/foggy.dialogue")
+const my_scene = preload("res://npc/foggy_dialogue.tscn")
+var in_body = false
+var balloon
+#use the example balloon to create an interaction
+#nready var balloon = $ExampleBalloon
+
+# dialogue resource? idfk//// dialogue start is the title of the dialogue
+@export var dialogue_resource: DialogueResource
+@export var dialogue_start: String = "start"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:	
+	
+	return
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Interact") && in_body:
+			var new_enemy = my_scene.instantiate()
+			add_child(new_enemy)
+			balloon = get_tree().get_first_node_in_group("hippo_dialogue")
+			if balloon != null:
+				balloon.start(resource, dialogue_start)
+	pass
+
+
+func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("player"):
+		in_body = true
+		print("FUCK")
+		
+	pass # Replace with function body.
+
+
+func _on_area_3d_body_shape_exited(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.is_in_group("player"):
+		in_body = false
+	pass # Replace with function body.
