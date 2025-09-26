fd22d0722f3ade116fe8cd276b1de836e7604765
Fathi Theolaire <fathi2.theolaire@live.uwe.ac.uk>
2025-09-26 04:49:17 +0100

    Hydration & Refill operational

--- j-imports/bloxy_cola.gd
+++ j-imports/bloxy_cola.gd
@@ -1,30 +1,31 @@
 extends Area3D
 
-@export var respawn_time = 5  # seconds
+@export var respawn_time = 15  
 @onready var respawn_timer: Timer = Timer.new()
+var active := true
 
 func _ready() -> void:
 	self.body_entered.connect(_on_body_entered)
+	
+	respawn_timer.one_shot = true
+	add_child(respawn_timer)
+	respawn_timer.timeout.connect(respawns)
 
 func _on_body_entered(body: Node) -> void:
+	if not active:
+		return
 	if body.is_in_group("player"):
 		var hydration = get_tree().root.get_meta("hydration")
-		if hydration:  # always check it's valid
+		if hydration:  
 			hydration.refill()
 			despawns()
 		
 func despawns() -> void:
+	active = false
 	visible = false
-	monitoring = false
-	respawn_timer.autostart
 	respawn_timer.start(respawn_time)
-	print("Despawn timer: " && respawn_timer.time_left)
-	respawn_timer.one_shot
-	if respawn_timer.is_stopped():
-		pass
-		
 	
 func respawns() -> void:
+	active = true
 	visible = true
-	monitoring = true
 	print("Respawned")
