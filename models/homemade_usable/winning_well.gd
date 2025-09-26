3d3f77a54eddc6c771ad14066524e7a419ae16e6
mlem <56688830+mlemmily@users.noreply.github.com>
2025-09-26 11:28:28 +0100

    final puush my beloved

--- models/homemade_usable/winning_well.gd
+++ models/homemade_usable/winning_well.gd
@@ -27,6 +27,7 @@ func _process(delta: float) -> void:
 func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
 		in_body = true
+		body.can_interact = true
 		print("FUCK")
 		
 	pass # Replace with function body.
@@ -34,5 +35,6 @@ func _on_area_3d_body_shape_entered(body_rid: RID, body: Node3D, body_shape_inde
 
 func _on_area_3d_body_shape_exited(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
+		body.can_interact = false
 		in_body = false
 	pass # Replace with function body.
